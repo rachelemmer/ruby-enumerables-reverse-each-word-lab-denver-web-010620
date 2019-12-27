0886ce0ce3b95e8146c869abe98a5fa09609824d
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-27 20:22:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
+  reversed_sentence = []
   new_sentence.each do |word| 
     reversed_sentence.push(word.reverse)
   end
+  reversed_sentence
 end 
