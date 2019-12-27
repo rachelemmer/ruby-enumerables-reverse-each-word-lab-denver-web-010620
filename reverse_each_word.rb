af369df1c284db004739314551d043787bd30bd9
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-27 20:35:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,9 @@ def reverse_each_word(sentence)
 end 
 
 def reverse_each_word(sentence)
-  
+  new_sentence = sentence.split
+  new_sentence.collect do |word|
+    word.reverse 
+  end 
+  new_sentence
+end 
